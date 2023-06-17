faa53237d36aa1112523598528d1f2683a1eddcf
edilbertoespinosa <edilbertoespinosa@gmail.com>
2023-06-17 17:53:51 -0600

    Se terminan de crear las rutas

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const app = express();
-const publicPath = path.resolve(__dirname,"../public");
+const publicPath = path.resolve(__dirname, "../public");
 
-app.listen(3000,console.log("Aplicacion levantada en puerto 3000"))
 app.use(express.static(publicPath));
 
-app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname,"../views/index.html"))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/login.html"));
+});
+
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/productCart.html"));
+});
+
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/productDetail.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../views/register.html"));
+});
+
+app.listen(3000, console.log("Aplicacion levantada en puerto 3000"));
