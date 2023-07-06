40c73347e49800997216c5b7315f6daba7820e82
Victoria Soto <vicky_yeah_12@hotmail.com>
2023-07-06 17:55:18 -0600

    creacion de archivos y reorganizacion de carpetas MVC

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -6,27 +6,27 @@ const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/index.html"));
+  res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
 app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/index.html"));
+  res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/login.html"));
+  res.sendFile(path.resolve(__dirname, "./views/users/login.html"));
 });
 
 app.get("/productCart", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/productCart.html"));
+  res.sendFile(path.resolve(__dirname, "./views/products/productCart.html"));
 });
 
 app.get("/productDetail", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/productDetail.html"));
+  res.sendFile(path.resolve(__dirname, "./views/products/productDetail.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../views/register.html"));
+  res.sendFile(path.resolve(__dirname, "./views/users/register.html"));
 });
 
 app.listen(3000, console.log("Aplicacion levantada http://localhost:3000"));
