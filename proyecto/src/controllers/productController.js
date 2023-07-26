5e16588f4087a2a74d27cbac32c07b740d7feae7
edilbertoespinosa <edilbertoespinosa@gmail.com>
2023-07-25 19:06:39 -0600

    Se levanta lectura de archivo JSON

--- proyecto/src/controllers/productController.js
+++ proyecto/src/controllers/productController.js
@@ -1,52 +1,61 @@
 const express = require("express");
 const path = require("path");
-const fs = require('fs');
-const mainController = require('./mainController')
+const fs = require("fs");
+const mainController = require("./mainController");
 const app = express();
+const archivoJSON = require("../data/archivoJSON");
 
-app.use(express.static(path.resolve(__dirname, './public')))
-app.set("view engine","ejs");
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.set("view engine", "ejs");
 
 const controller = {
-    productDetail: function(req, res) {
-        res.render('products/productDetail')
-    },
+  productDetail: function (req, res) {
+    const id = parseInt(req.params.id);
+    const datos = archivoJSON.leerJson("products.json");
+    const dato = datos.filter((dato) => dato.id === id);
+    res.send(dato);
+    //res.send(dato.nombre);
+    //res.render("products/productDetail", { dato });
+  },
 
-    productCart: function(req, res) {
-        res.render('products/productCart');
-    },
+  productCart: function (req, res) {
+    res.render("products/productCart");
+  },
 
-    productList: function(req,res){
-        res.render('products/productList')
-    },
+  productList: function (req, res) {
+    res.render("products/productList");
+  },
 
-    createProduct: function(req,res){
-        res.render('products/createProducts')
-    },
+  createProduct: function (req, res) {
+    res.render("products/createProducts");
+  },
 
-    save: (req, res) => {
-        let peliculas = mainController.leerJson("products.json")
-        let nuevaPelicula = {
-            id : peliculas[peliculas.length -1].id + 1,
-            nombre: req.body.nombre,
-            genero: req.body.genero,
-            duracion: req.body.duracion,
-            año: req.body.año,
-            clasificacionEdad: req.body.clasificacionEdad,
-            descripcion: req.body.descripcion,
-            advertenciaContenido: req.body.advertenciaContenido,
-            director: req.body.director,
-            reparto: req.body.reparto,
-            estudio: req.body.estudio,
-            subtitulos: req.body.subtitulos,
-            precio: req.body.precio,
-            imagen: req.file.filename
-        }
-        peliculas.push(nuevaPelicula);
-        let nuevaPelciulaGuardar = JSON.stringify(peliculas, null, 2)
-        fs.writeFileSync(path.resolve(__dirname, '../data/products.json'), nuevaPelciulaGuardar)
-        res.redirect('/products/productList')
-    }
-}
+  save: (req, res) => {
+    let peliculas = mainController.leerJson("products.json");
+    let nuevaPelicula = {
+      id: peliculas[peliculas.length - 1].id + 1,
+      nombre: req.body.nombre,
+      genero: req.body.genero,
+      duracion: req.body.duracion,
+      año: req.body.año,
+      clasificacionEdad: req.body.clasificacionEdad,
+      descripcion: req.body.descripcion,
+      advertenciaContenido: req.body.advertenciaContenido,
+      director: req.body.director,
+      reparto: req.body.reparto,
+      estudio: req.body.estudio,
+      subtitulos: req.body.subtitulos,
+      precio: req.body.precio,
+      imagen: req.file.filename,
+    };
+    peliculas.push(nuevaPelicula);
+    let nuevaPelciulaGuardar = JSON.stringify(peliculas, null, 2);
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/products.json"),
+      nuevaPelciulaGuardar
+    );
+    res.redirect("/products/productList");
+  },
+};
 
 module.exports = controller;
--- proyecto/src/routes/products.js
+++ proyecto/src/routes/products.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
-const multer = require('multer');
+const path = require("path");
+const multer = require("multer");
 const productsController = require("../controllers/productController");
 
 //subir archivo y nombre
 var storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null, path.resolve(__dirname,'../../public/images/peliculas'))
-    },
-    filename: function(req,file,cb){
-        cb(null, 'pelicula-' + Date.now() + path.extname(file.originalname))
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, "../../public/images/peliculas"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, "pelicula-" + Date.now() + path.extname(file.originalname));
+  },
+});
 
-const upload = multer({storage})
+const upload = multer({ storage });
 
-router.get('/productDetail', productsController.productDetail);
-router.get('/productCart', productsController.productCart);
-router.get('/productList', productsController.productList);
-router.get('/createProducts', productsController.createProduct);
-router.post('/createProducts', upload.single('imagen'), productsController.save);
+router.get("/productDetail/:id", productsController.productDetail);
+router.get("/productCart", productsController.productCart);
+router.get("/productList", productsController.productList);
+router.get("/createProducts", productsController.createProduct);
+router.post(
+  "/createProducts",
+  upload.single("imagen"),
+  productsController.save
+);
 
 module.exports = router;
