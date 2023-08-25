0d230bcadbd0b8f36f7b9256f32768b0b937538c
edilbertoespinosa <edilbertoespinosa@gmail.com>
2023-08-24 22:33:41 -0600

    Se convigura session

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 const publicPath = path.resolve(__dirname, "../public");
 const rutaMain = require("./routes/main");
 const rutaUsers = require("./routes/users");
 const rutaProducts = require("./routes/products");
 const rutaAdmin = require("./routes/admin");
+const session = require("express-session");
 
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.static(publicPath));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use("/", rutaMain);
 app.use("/users", rutaUsers);
 app.use("/products", rutaProducts);
 app.use("/admin", rutaAdmin);
+app.use(session({ secret: "supercalifragilisticoespialidoso" }));
 
 app.listen(3000, console.log("Aplicacion levantada http://localhost:3000"));
